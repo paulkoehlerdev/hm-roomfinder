27ca0b35a58186101de1543f57a3a11bf5fe3a52
Michael Albrecht <michael.j.a@hotmail.de>
2024-06-15 12:30:28 +0200

    added function to call api if user moves the map

--- app/lib/full_map_page.dart
+++ app/lib/full_map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
+import 'dart:math';
 
 class FullMap extends StatefulWidget {
   const FullMap({super.key});
@@ -10,6 +11,21 @@ class FullMap extends StatefulWidget {
 
 class FullMapState extends State<FullMap> {
   MapLibreMapController? mapController;
+
+double pythLatLong(pose1, pose2) {
+  return sqrt(pow(pose1.latitude - pose2.latitude, 2) + pow(pose1.longitude - pose2.longitude, 2));
+}
+
+void cameraListener(controller) {
+    LatLng oldCameraPose = LatLng(0.0, 0.0);
+    controller!.addListener(() {
+      if (controller!.cameraPosition != null){
+      if (pythLatLong(oldCameraPose, controller!.cameraPosition!.target) > 0.1 && controller!.cameraPosition!.zoom > 12.0){
+        oldCameraPose = controller!.cameraPosition!.target;
+        print('Camera moved to pose ${controller!.cameraPosition!.target}');
+      }}
+    });
+}
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +41,7 @@ class FullMapState extends State<FullMap> {
       initialCameraPosition: const CameraPosition(bearing: 0.0, target: LatLng(51.8, 9.7), tilt: 0.0, zoom: 5.5),
       trackCameraPosition: true,
       onMapCreated: (controller) {
-            /*controller.addListener(() {
-              print(controller.cameraPosition);
-            });*/
+        cameraListener(controller);
             controller.requestMyLocationLatLng().then((value)
               => value != null ? controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: value, zoom: 17.0))) : null);
           },
