f76dca9ee2c17f6087c0aab1721db157f98ac29d
Michael Albrecht <michael.j.a@hotmail.de>
2024-06-16 13:55:44 +0200

    changed del_rooms to generally del all room layers

--- app/lib/full_map_page.dart
+++ app/lib/full_map_page.dart
@@ -4,14 +4,49 @@ import 'package:maplibre_gl/maplibre_gl.dart';
 import 'dart:math';
 import 'package:provider/provider.dart';
 
-Map<String,dynamic> geojson = {
-"type": "FeatureCollection",
-"name": "example_geojson",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "pk": 1, "name": "F0.02", "storey_id": 1 }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 11.568194183434708, 48.142868235160421 ], [ 11.568301598509459, 48.142837212756163 ], [ 11.568232379769954, 48.142731154911601 ], [ 11.568124770805175, 48.142762565095914 ], [ 11.568194183434708, 48.142868235160421 ] ] ] } },
-{ "type": "Feature", "properties": { "pk": 2, "name": "F0.01a", "storey_id": 1 }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 11.568193989544682, 48.142868429050452 ], [ 11.568301210729405, 48.142837406646187 ], [ 11.568320793622096, 48.142869204610555 ], [ 11.568290934557995, 48.142877541881703 ], [ 11.568292485678208, 48.142881419682233 ], [ 11.568216868567823, 48.142902941475192 ], [ 11.568193989544682, 48.142868429050452 ] ] ] } },
-]
+Map<String, dynamic> geojson = {
+  "type": "FeatureCollection",
+  "name": "example_geojson",
+  "crs": {
+    "type": "name",
+    "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84"}
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {"pk": 1, "name": "F0.02", "storey_id": 1},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [11.568194183434708, 48.142868235160421],
+            [11.568301598509459, 48.142837212756163],
+            [11.568232379769954, 48.142731154911601],
+            [11.568124770805175, 48.142762565095914],
+            [11.568194183434708, 48.142868235160421]
+          ]
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {"pk": 2, "name": "F0.01a", "storey_id": 1},
+      "geometry": {
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [11.568193989544682, 48.142868429050452],
+            [11.568301210729405, 48.142837406646187],
+            [11.568320793622096, 48.142869204610555],
+            [11.568290934557995, 48.142877541881703],
+            [11.568292485678208, 48.142881419682233],
+            [11.568216868567823, 48.142902941475192],
+            [11.568193989544682, 48.142868429050452]
+          ]
+        ]
+      }
+    },
+  ]
 };
 
 final geojsonSource = GeojsonSourceProperties(
@@ -34,59 +69,84 @@ class FullMap extends StatefulWidget {
 class FullMapState extends State<FullMap> {
   MapLibreMapController? mapController;
 
-double pythLatLong(pose1, pose2) {
-  return sqrt(pow(pose1.latitude - pose2.latitude, 2) + pow(pose1.longitude - pose2.longitude, 2));
-}
+  double pythLatLong(pose1, pose2) {
+    return sqrt(pow(pose1.latitude - pose2.latitude, 2) +
+        pow(pose1.longitude - pose2.longitude, 2));
+  }
 
-void cameraListener(controller, updateZoomLevelProvider) {
+  void cameraListener(controller, updateZoomLevelProvider) {
     LatLng oldCameraPose = LatLng(0.0, 0.0);
     double oldZoom = 0.0;
-    double zoomThreshold = 14.0;
+    double zoomThreshold = 15.0;
     double locationThreshold = 0.1;
     bool zoomChanged = false;
     bool locationChanged = false;
     controller!.addListener(() {
-      if (controller!.cameraPosition != null){
-        zoomChanged = oldZoom > zoomThreshold && controller!.cameraPosition!.zoom < zoomThreshold || oldZoom < zoomThreshold && controller!.cameraPosition!.zoom > zoomThreshold;
-        locationChanged = pythLatLong(oldCameraPose, controller!.cameraPosition!.target) > locationThreshold;
-        if (locationChanged || zoomChanged ){
+      if (controller!.cameraPosition != null) {
+        zoomChanged = oldZoom > zoomThreshold &&
+                controller!.cameraPosition!.zoom < zoomThreshold ||
+            oldZoom < zoomThreshold &&
+                controller!.cameraPosition!.zoom > zoomThreshold;
+        locationChanged =
+            pythLatLong(oldCameraPose, controller!.cameraPosition!.target) >
+                locationThreshold;
+        if (locationChanged || zoomChanged) {
           // if zooming in
-          if (controller!.cameraPosition!.zoom > zoomThreshold){
+          if (controller!.cameraPosition!.zoom > zoomThreshold) {
             oldCameraPose = controller!.cameraPosition!.target;
             loadRooms(controller!.cameraPosition!.target, locationThreshold);
             oldZoom = controller!.cameraPosition!.zoom;
-            updateZoomLevelProvider.updateZoomLevel(controller!.cameraPosition!.zoom > zoomThreshold);
-            zoomChanged = false; 
-            locationChanged = false;
-          // if zooming out
-          } else {
-            oldCameraPose = controller!.cameraPosition!.target;
-            delRooms(controller!.cameraPosition!.target); 
-            oldZoom = controller!.cameraPosition!.zoom;
-            updateZoomLevelProvider.updateZoomLevel(controller!.cameraPosition!.zoom > zoomThreshold);
+            updateZoomLevelProvider.updateZoomLevel(
+                controller!.cameraPosition!.zoom > zoomThreshold);
             zoomChanged = false;
             locationChanged = false;
-        }
+            // if zooming out
+          } else {
+            oldCameraPose = controller!.cameraPosition!.target;
+            delRooms();
+            oldZoom = controller!.cameraPosition!.zoom;
+            updateZoomLevelProvider.updateZoomLevel(
+                controller!.cameraPosition!.zoom > zoomThreshold);
+            zoomChanged = false;
+            locationChanged = false;
+          }
         }
       }
     });
-}
-
-void loadRooms(LatLng cameraPosition, double locationThreshold){
-  //example geojson location
-  LatLng geojsonLoc = const LatLng(48.142868235160421, 11.568194183434708);
-  if (pythLatLong(cameraPosition, geojsonLoc) < locationThreshold){
-    //adding the example geojson
-    mapController!.addSource('example_geojson', geojsonSource);
-    mapController!.addFillLayer('example_geojson', 'example_geojson', fillLayer);
   }
-}
 
-void delRooms(LatLng cameraPosition){
-  //example geojson
-  mapController!.removeLayer('example_geojson');
-  mapController!.removeSource('example_geojson');
-}
+  void loadRooms(LatLng cameraPosition, double locationThreshold) {
+    //example geojson location
+    LatLng geojsonLoc = const LatLng(48.142868235160421, 11.568194183434708);
+    if (pythLatLong(cameraPosition, geojsonLoc) < locationThreshold) {
+      //adding the example geojson
+      mapController!.addSource('room_example_geojson', geojsonSource);
+      mapController!.addFillLayer(
+          'room_example_geojson', 'room_example_geojson', fillLayer);
+    }
+  }
+
+  void delRooms() async {
+    mapController!.getLayerIds().then((layerIds) {
+      //example geojson
+      for (String layer in layerIds) {
+        if (layer.length >= 4) {
+          if (layer.substring(0, 4) == 'room') {
+            mapController!.removeLayer(layer);
+          }
+        }
+      }
+    });
+    mapController!.getSourceIds().then((sourceIds) {
+      for (String source in sourceIds) {
+        if (source.length >= 4) {
+          if (source.substring(0, 4) == 'room') {
+            mapController!.removeSource(source);
+          }
+        }
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -99,24 +159,30 @@ void delRooms(LatLng cameraPosition){
     return Scaffold(
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            mapController!.requestMyLocationLatLng().then((value)
-              => value != null ? mapController!.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: value, zoom: 17.0))) : null);
+            mapController!.requestMyLocationLatLng().then((value) => value !=
+                    null
+                ? mapController!.animateCamera(CameraUpdate.newCameraPosition(
+                    CameraPosition(target: value, zoom: 17.0)))
+                : null);
           },
           child: const Icon(Icons.my_location),
         ),
         body: MapLibreMap(
-      myLocationEnabled: true,
-      myLocationTrackingMode: MyLocationTrackingMode.trackingCompass,
-      myLocationRenderMode: MyLocationRenderMode.compass,
-      styleString: styleUrl,
-      initialCameraPosition: const CameraPosition(bearing: 0.0, target: LatLng(51.8, 9.7), tilt: 0.0, zoom: 5.5),
-      trackCameraPosition: true,
-      onMapCreated: (controller) {
-        mapController = controller;
-        cameraListener(controller, updateZoomLevelProvider);
-            controller.requestMyLocationLatLng().then((value)
-              => value != null ? controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: value, zoom: 17.0))) : null); 
+          myLocationEnabled: true,
+          myLocationTrackingMode: MyLocationTrackingMode.trackingCompass,
+          myLocationRenderMode: MyLocationRenderMode.compass,
+          styleString: styleUrl,
+          initialCameraPosition: const CameraPosition(
+              bearing: 0.0, target: LatLng(51.8, 9.7), tilt: 0.0, zoom: 5.5),
+          trackCameraPosition: true,
+          onMapCreated: (controller) {
+            mapController = controller;
+            cameraListener(controller, updateZoomLevelProvider);
+            controller.requestMyLocationLatLng().then((value) => value != null
+                ? controller.animateCamera(CameraUpdate.newCameraPosition(
+                    CameraPosition(target: value, zoom: 17.0)))
+                : null);
           },
-    ));
+        ));
   }
 }
