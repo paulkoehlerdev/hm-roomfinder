20de270d107e5f3c886dd38142c6aba90f39e678
Michael Albrecht <michael.j.a@hotmail.de>
2024-06-16 11:29:22 +0200

    added provider to show zoombar only if needed

--- app/lib/full_map_page.dart
+++ app/lib/full_map_page.dart
@@ -1,6 +1,8 @@
+import 'package:app/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:maplibre_gl/maplibre_gl.dart';
 import 'dart:math';
+import 'package:provider/provider.dart';
 
 Map<String,dynamic> geojson = {
 "type": "FeatureCollection",
@@ -36,21 +38,43 @@ double pythLatLong(pose1, pose2) {
   return sqrt(pow(pose1.latitude - pose2.latitude, 2) + pow(pose1.longitude - pose2.longitude, 2));
 }
 
-void cameraListener(controller) {
+void cameraListener(controller, updateZoomLevelProvider) {
     LatLng oldCameraPose = LatLng(0.0, 0.0);
+    double oldZoom = 0.0;
+    double zoomThreshold = 14.0;
     controller!.addListener(() {
       if (controller!.cameraPosition != null){
-      if (pythLatLong(oldCameraPose, controller!.cameraPosition!.target) > 0.1 && controller!.cameraPosition!.zoom > 12.0){
+      if (pythLatLong(oldCameraPose, controller!.cameraPosition!.target) > 0.1 && controller!.cameraPosition!.zoom > 14.0){
         oldCameraPose = controller!.cameraPosition!.target;
         print('Camera moved to pose ${controller!.cameraPosition!.target}');
+        loadRooms(controller!.cameraPosition!.target); 
       }}
+      if (oldZoom > zoomThreshold && controller!.cameraPosition!.zoom < zoomThreshold 
+        || oldZoom < zoomThreshold && controller!.cameraPosition!.zoom > zoomThreshold){
+        oldZoom = controller!.cameraPosition!.zoom;
+        updateZoomLevelProvider.updateZoomLevel(controller!.cameraPosition!.zoom > zoomThreshold);
+        print('Zoom changed to ${controller!.cameraPosition!.zoom}');
+      }
     });
+}
+
+void loadRooms(LatLng cameraPosition){
+  //example geojson location
+  LatLng geojsonLoc = const LatLng(48.142868235160421, 11.568194183434708);
+  if (pythLatLong(cameraPosition, geojsonLoc) < 0.1){
+    //adding the example geojson
+    mapController!.addSource('example_geojson', geojsonSource);
+    mapController!.addFillLayer('example_geojson', 'example_geojson', fillLayer);
+  }
 }
 
   @override
   Widget build(BuildContext context) {
     const styleUrl =
         'https://raw.githubusercontent.com/go2garret/maps/main/src/assets/json/openStreetMap.json';
+
+    final ZoomLevelProvider updateZoomLevelProvider =
+        Provider.of<ZoomLevelProvider>(context, listen: false);
 
     return Scaffold(
         floatingActionButton: FloatingActionButton(
@@ -69,12 +93,9 @@ void cameraListener(controller) {
       trackCameraPosition: true,
       onMapCreated: (controller) {
         mapController = controller;
-        cameraListener(controller);
+        cameraListener(controller, updateZoomLevelProvider);
             controller.requestMyLocationLatLng().then((value)
-              => value != null ? controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: value, zoom: 17.0))) : null);
-            //adding the example geojson
-            controller.addSource('example_geojson', geojsonSource);
-            controller.addFillLayer('example_geojson', 'example_geojson', fillLayer);
+              => value != null ? controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: value, zoom: 17.0))) : null); 
           },
     ));
   }
--- app/lib/home_view.dart
+++ app/lib/home_view.dart
@@ -1,20 +1,28 @@
 import 'package:app/full_map_page.dart';
 import 'package:app/level_selector.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'providers.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            FullMap(),
-            LevelSelector(),
-          ],
-      ),
-    );
+    return ChangeNotifierProvider<ZoomLevelProvider>(
+        create: (context) => ZoomLevelProvider(),
+        child: Builder(builder: (BuildContext context) {
+          final zoomLevelProvider = Provider.of<ZoomLevelProvider>(context);
+          return Material(
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                FullMap(),
+                zoomLevelProvider.zoomLevel ? const LevelSelector() : Container(),
+              ],
+            ),
+          );
+        }));
   }
 }
