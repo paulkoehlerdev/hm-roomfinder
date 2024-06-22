c412d0b61b49d28430ca6ae312c67586d40cc207
Paul Köhler <koehler_paul@outlook.de>
2024-06-22 16:44:23 +0200

    Fix meili format

--- backend/pkg/geodata/domain/entities/document/document.go
+++ backend/pkg/geodata/domain/entities/document/document.go
@@ -3,7 +3,19 @@ package document
 import "github.com/paulkoehlerdev/hm-roomfinder/backend/pkg/geodata/domain/entities/geojson"
 
 type Document struct {
-	Data map[string]interface{}   `mapstructure:",remain"`
-	Geo  geojson.CoordinatesPoint `mapstructure:"_geo"`
-	Id   int64                    `mapstructure:"_doc_id"`
+	Data map[string]interface{} `mapstructure:",remain"`
+	Geo  Geo                    `mapstructure:"_geo"`
+	Id   int64                  `mapstructure:"_doc_id"`
+}
+
+type Geo struct {
+	Lat float64 `mapstructure:"lat"`
+	Lng float64 `mapstructure:"lng"`
+}
+
+func GeoFromGeojsonPoint(point geojson.GeometryPoint) Geo {
+	return Geo{
+		Lat: point.Coordinates[0],
+		Lng: point.Coordinates[1],
+	}
 }
