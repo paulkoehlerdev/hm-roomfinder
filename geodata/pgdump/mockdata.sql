dde4ab0cddf2b0abc2db7d496ed2144f8877f987
Paul Köhler <koehler_paul@outlook.de>
2024-06-22 11:44:18 +0200

    Fixed error in sql code

--- geodata/pgdump/mockdata.sql
+++ geodata/pgdump/mockdata.sql
@@ -2,7 +2,7 @@ INSERT INTO level (building_id, name, attr, geom) SELECT building_id, '1.OG' as 
 INSERT INTO level (building_id, name, attr, geom) SELECT building_id, '2.OG' as name, attr, geom  FROM level WHERE id=1;
 
 INSERT INTO room (level_id, name, attr, geom)
-SELECT 2 as level_id, concat('F1.', substring(name FROM 4)) as name, attr, geom FROM room;
+SELECT 2 as level_id, concat('F1.', substring(name FROM 4)) as name, attr, geom FROM room WHERE level_id = 1;
 
 INSERT INTO room (level_id, name, attr, geom)
-SELECT 3 as level_id, concat('F2.', substring(name FROM 4)) as name, attr, geom FROM room;
+SELECT 3 as level_id, concat('F2.', substring(name FROM 4)) as name, attr, geom FROM room WHERE level_id = 1;
