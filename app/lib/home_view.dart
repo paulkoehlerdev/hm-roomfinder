9c6e8d1316cd6868408082ec7779efe292b16f95
Michael Albrecht <michael.j.a@hotmail.de>
2024-07-02 10:58:34 +0200

    added ability to test app without loading the map

--- app/lib/home_view.dart
+++ app/lib/home_view.dart
@@ -1,19 +1,20 @@
 import 'package:hm_roomfinder/components/search_bar_component.dart';
 import 'package:hm_roomfinder/components/full_map_page.dart';
 import 'package:flutter/material.dart';
+import 'util/globals.dart' as globals;
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Material(
         child: Stack(
           alignment: Alignment.bottomCenter,
           children: [
-            FullMap(),
-            SearchBarComponent(),
+            globals.testMode ? Container() : const FullMap(),
+            const SearchBarComponent(),
           ],
         ),
       ),
---
+++ app/lib/util/globals.dart
@@ -0,0 +1,2 @@
+
+bool testMode = false;
