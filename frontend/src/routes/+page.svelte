9a734d5561c70bc946e2b8b0651b653e9f728c7a
Paul Köhler <koehler_paul@outlook.de>
2024-07-22 05:46:55 +0200

    Feat: Added shadcn/ui

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Sun from "lucide-svelte/icons/sun";
+    import Moon from "lucide-svelte/icons/moon";
+
+    import {toggleMode} from "mode-watcher";
+    import {Button} from "$lib/components/ui/button/index.js";
+</script>
+
+<Button on:click={toggleMode} variant="outline" size="icon">
+    <Sun
+            class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+    />
+    <Moon
+            class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    />
+    <span class="sr-only">Toggle theme</span>
+</Button>
